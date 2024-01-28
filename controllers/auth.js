8f130f45649aa7fca966fb7fd3539419553a2185
Noman Rafiq <heyimnomee@gmail.com>
2024-01-28 11:23:31 +0500

    hashed passwords and preSave

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 const UserModel = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const bycrypt = require('bcryptjs'); //hashing the password
 
 const register = async (req, res) => {
     const user = await UserModel.create({ ...req.body });
-    return res.status(StatusCodes.CREATED).json({ user });
+    return res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bycrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -14,14 +15,22 @@ const UserSchema = new mongoose.Schema({
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please provide valid email',
         ],
-        unique: true,
+        unique: [true, 'Email already exists.'],
     },
     password: {
         type: String,
         required: [true, 'Please provide password'],
         minlength: 6,
-        maxlength: 12,
     },
+});
+
+//Pre 'Save' Middleware to hash passwords before you save it
+// we will use normal function syntax because we need access to 'this' method
+// in the later update we can even remove the next() and its still going to work
+UserSchema.pre('save', async function (next) {
+    const salt = await bycrypt.genSalt(10);
+    this.password = await bycrypt.hash(this.password, salt);
+    next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
